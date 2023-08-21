a466f67d675bc5359d63fc4dbdd6a850c137e4ec
Arthur DANJOU <arthurdanjou@outlook.fr>
2023-08-21 16:38:19 +0200

    trying to fix the duplicata

--- src/error.vue
+++ src/error.vue
@@ -5,36 +5,26 @@ const getColor = computed(() => appConfig.ui.primary)
 </script>
 
 <template>
-  <NuxtLoadingIndicator :color="getColor" />
-  <section class="fixed inset-0 flex justify-center sm:px-8">
-    <div class="flex w-full max-w-7xl">
-      <div class="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+  <NuxtLayout name="default">
+    <div class="flex flex-col items-center gap-4">
+      <h1 class="font-medium text-[8rem] md:text-[16rem] leading-none bg-error bg-clip-text tracking-wider font-error" :class="`text-${getColor}-500`">
+        {{ error.statusCode }}
+      </h1>
+      <p class="text-lg md:text-2xl text-subtitle text-center">
+        Sorry, {{ error.statusCode === 404
+          ? "the page you are looking for doesn't exist or as been moved."
+          : "you have encountered a problem."
+        }}
+        <br>
+        Let's find a better place for you to go.
+      </p>
     </div>
-  </section>
-  <div class="relative z-50">
-    <Header />
-    <UContainer class="my-32 w-container flex flex-col items-center gap-8">
-      <div class="flex flex-col items-center gap-4">
-        <h1 class="font-medium text-[8rem] md:text-[16rem] leading-none bg-error bg-clip-text tracking-wider font-error" :class="`text-${getColor}-500`">
-          {{ error.statusCode }}
-        </h1>
-        <p class="text-lg md:text-2xl text-subtitle text-center">
-          Sorry, {{ error.statusCode === 404
-            ? "the page you are looking for doesn't exist or as been moved."
-            : "you have encountered a problem."
-          }}
-          <br>
-          Let's find a better place for you to go.
-        </p>
-      </div>
-      <div>
-        <UButton to="/" size="md" variant="soft" color="primary">
-          Go back to the main page
-        </UButton>
-      </div>
-    </UContainer>
-    <Footer />
-  </div>
+    <div>
+      <UButton to="/" size="md" variant="soft" color="primary">
+        Go back to the main page
+      </UButton>
+    </div>
+  </NuxtLayout>
 </template>
 
 <style scoped>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-const days = ref(0)
 useHead({
   title: 'Arthur Danjou • Software Engineer and Maths Lover',
 })
