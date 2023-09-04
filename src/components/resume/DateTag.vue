a24ed4653b00811b577c7cbfef61ab58fa17d38e
Arthur DANJOU <arthurdanjou@outlook.fr>
2023-09-04 13:42:05 +0200

    Improve datetag style

--- src/components/resume/DateTag.vue
+++ src/components/resume/DateTag.vue
@@ -14,16 +14,10 @@ function formatTodayDate(date: string) {
 </script>
 
 <template>
-  <div
-    v-if="startDate !== endDate"
-    class="mb-1 translate-y-px inline-block flex-none rounded bg-zinc-200 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400"
-  >
+  <UBadge v-if="startDate === endDate" variant="soft" size="xs">
     {{ formatTodayDate(startDate!.toString()) }} — {{ formatTodayDate(endDate) }}
-  </div>
-  <div
-    v-else
-    class="mb-1 translate-y-px inline-block flex-none rounded bg-zinc-200 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400"
-  >
+  </UBadge>
+  <UBadge v-else variant="soft" size="xs">
     {{ formatTodayDate(endDate) }}
-  </div>
+  </UBadge>
 </template>
