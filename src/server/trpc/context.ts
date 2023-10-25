9486a0549ab2cda72b5d33360df283273b8985c9
Arthur DANJOU <arthurdanjou@outlook.fr>
2023-10-25 20:20:41 +0200

    Change prisma log

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,13 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import type { inferAsyncReturnType } from '@trpc/server'
 import type { H3Event } from 'h3'
-import { PrismaClient } from '@prisma/client'
 
 let prisma: PrismaClient | undefined
 
 export function createContext(_event: H3Event) {
   if (!prisma) {
     prisma = new PrismaClient({
-      log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+      log: ['warn', 'info', 'error'],
     })
   }
 
