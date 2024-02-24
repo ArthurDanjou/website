31031e931a26d79c92ca2bda14ddeda757535b70
Arthur DANJOU <arthurdanjou@outlook.fr>
2024-02-24 01:12:56 +0100

    Add background style

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -19,10 +19,11 @@ onMounted(() => configureGraphs({ darkMode: colorMode.value === 'dark' }))
 <template>
   <div>
     <SpeedInsights />
+    <Background />
     <NuxtLoadingIndicator :color="getColor" />
     <section class="fixed inset-0 flex justify-center sm:px-8">
       <div class="flex w-full max-w-7xl">
-        <div class="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        <div class="w-full z-20 bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
       </div>
     </section>
     <main class="relative z-50 min-h-[100svh]">
