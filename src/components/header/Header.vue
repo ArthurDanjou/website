6860c328fadfe918e2b11f57e5ad612dd5798cf8
Arthur DANJOU <arthurdanjou@outlook.fr>
2024-02-26 20:32:02 +0100

    Fix mobile ivew for header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="z-30 sticky top-0 left-0 flex justify-center w-full">
-    <div class="w-full px-4 sm:px-6 lg:px-8 sm:mx-8 max-w-7xl py-4 grid grid-cols-2 lg:grid-cols-3 md:grid-cols-3 bg-white dark:bg-zinc-900 border-b border-zinc-100 dark:border-zinc-300/10">
+    <div class="w-full px-4 sm:px-6 lg:px-8 sm:mx-8 max-w-7xl py-4 grid grid-cols-2 lg:grid-cols-3 bg-white dark:bg-zinc-900 border-b border-zinc-100 dark:border-zinc-300/10">
       <Logo />
-      <div class="grow flex justify-center">
+      <div class="hidden grow lg:flex justify-center">
         <NavBar />
       </div>
       <div class="flex justify-end gap-2 items-center">
